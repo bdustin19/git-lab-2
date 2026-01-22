bab4ae8099d50b595324e8805e17e00467f3cece
Blake <bd125225@ohio.edu>
2026-01-21 19:34:06 -0500

    File Hello Git

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Blake Dustin
+ *        Date: 1/21/25
+ * Description: File for Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" <<endl;
+    return 0;
+}// main
